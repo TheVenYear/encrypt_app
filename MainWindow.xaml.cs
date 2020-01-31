5f1fab87d33eac36899ea507df75dabae5254a25
654-17 <654-17@C41205>
2020-01-31 10:40:45 +0300

    выфафыа

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace encrypt_app
 {
     public partial class MainWindow : Window
     {
+        Dictionary<int, char> AlphArray = new Dictionary<int, char>();
         public MainWindow()
         {
             InitializeComponent();
@@ -24,28 +25,72 @@ namespace encrypt_app
 
         private void Encrypt_Click(object sender, RoutedEventArgs e)
         {
-            Dictionary<int, char> AlphArray = new Dictionary<int, char>();
+            AlphArray = new Dictionary<int, char>();
             var randomArray = new List<int>();
+
             for (int i = 0; i < input.Text.Length; i++)
             {
                 AlphArray.Add(i, input.Text[i]);
                 randomArray.Add(i);
             }
-
             var random = new Random(DateTime.Now.Millisecond);
             randomArray = randomArray.OrderBy(x => random.Next()).ToList();
 
             List<char> chars = Enumerable.ToList(AlphArray.Values);
             List<int> vals = Enumerable.ToList(AlphArray.Keys);
-            var lines = new string[2];
 
+            output.Text = "";
+            outputVars.Text = "";
             for (int i = 0; i < AlphArray.Count; i++)
             {
-                lines[0] += chars[randomArray[i]];
-                lines[1] += vals[randomArray[i]] + 1;
+                output.Text += chars[randomArray[i]];
+                outputVars.Text += $"{vals[randomArray[i]] + 1} ";
             }
-            output.Text = lines[0];
-            outputVars.Text = lines[1];
+
+        }
+
+        private void Decrypt_Click(object sender, RoutedEventArgs e)
+        {
+            if (output.Text != "" && outputVars.Text != "")
+            {
+                var arrNums = new List<int>();
+                var arrChars = new List<char>();
+                AlphArray = new Dictionary<int, char>();
+                foreach (var item in output.Text)
+                {
+                    arrChars.Add(item);
+                }
+
+                var element = "";
+                var counter = 0;
+                foreach (var item in outputVars.Text)
+                {
+                    if (item == ' ')
+                    {
+                        arrNums.Add(int.Parse(element.ToString()) - 1);
+                        counter++;
+                        element = "";
+                    }
+                    element += item;
+                }
+
+                for (int i = 0; i < arrNums.Count; i++)
+                {
+                    AlphArray.Add(arrNums[i], arrChars[i]);
+                }
+                var sorted = new SortedDictionary<int, char>(AlphArray);
+
+
+
+                foreach (var item in sorted.Values)
+                {
+                    element += $"{item}";
+                }
+                input.Text = element;
+            }
+            else
+                MessageBox.Show("Неверные данные");
+
         }
     }
 }
